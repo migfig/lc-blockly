18ddf8afd3ae693982099ae7444eaa08a4413cb1
Miguel Figueroa <migfig@github.com>
2023-06-30 17:49:01 -0600

    Updates

---
+++ precache-manifest.a8c5ba3576cd3eeb5c081cf68c88fa49.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c2e931ed6262d00922da06a7e24910",
+    "url": "/lc-blockly/index.html"
+  },
+  {
+    "revision": "d3509e90e99f94e10390",
+    "url": "/lc-blockly/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bf1851324e753905a26b",
+    "url": "/lc-blockly/static/css/main.7ebf5ef0.chunk.css"
+  },
+  {
+    "revision": "d3509e90e99f94e10390",
+    "url": "/lc-blockly/static/js/2.a1ad0a2b.chunk.js"
+  },
+  {
+    "revision": "07fdafb0f100b712db4c60416767c7d5",
+    "url": "/lc-blockly/static/js/2.a1ad0a2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf1851324e753905a26b",
+    "url": "/lc-blockly/static/js/main.caec58c8.chunk.js"
+  },
+  {
+    "revision": "56140060a6c80d534522cd0dd4cde851",
+    "url": "/lc-blockly/static/js/main.caec58c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ecc48c1b27d5421c849",
+    "url": "/lc-blockly/static/js/runtime-main.bff7e4c1.js"
+  },
+  {
+    "revision": "6eea6d8ab01207d9af4b8c0e78e4c832",
+    "url": "/lc-blockly/static/media/LearningRateJustRight.6eea6d8a.svg"
+  },
+  {
+    "revision": "a1b26c89bdfe258bfdc96c541de701dd",
+    "url": "/lc-blockly/static/media/LearningRateTooLarge.a1b26c89.svg"
+  },
+  {
+    "revision": "0d1a77a6360570f295195f9f6f6edf9a",
+    "url": "/lc-blockly/static/media/LearningRateTooSmall.0d1a77a6.svg"
+  },
+  {
+    "revision": "03f03941f0ecefabc2e648dd8b63cd05",
+    "url": "/lc-blockly/static/media/application.03f03941.py"
+  },
+  {
+    "revision": "6600543ed5b9e82f1f73cb881a283fea",
+    "url": "/lc-blockly/static/media/custom_chain_field.6600543e.py"
+  },
+  {
+    "revision": "6600543ed5b9e82f1f73cb881a283fea",
+    "url": "/lc-blockly/static/media/custom_chat_message_field.6600543e.py"
+  },
+  {
+    "revision": "6600543ed5b9e82f1f73cb881a283fea",
+    "url": "/lc-blockly/static/media/custom_chat_model_field.6600543e.py"
+  },
+  {
+    "revision": "6600543ed5b9e82f1f73cb881a283fea",
+    "url": "/lc-blockly/static/media/custom_chat_prompt_field.6600543e.py"
+  },
+  {
+    "revision": "6600543ed5b9e82f1f73cb881a283fea",
+    "url": "/lc-blockly/static/media/custom_embedding_field.6600543e.py"
+  },
+  {
+    "revision": "6600543ed5b9e82f1f73cb881a283fea",
+    "url": "/lc-blockly/static/media/custom_llm_field.6600543e.py"
+  },
+  {
+    "revision": "6600543ed5b9e82f1f73cb881a283fea",
+    "url": "/lc-blockly/static/media/custom_loader_field.6600543e.py"
+  },
+  {
+    "revision": "6600543ed5b9e82f1f73cb881a283fea",
+    "url": "/lc-blockly/static/media/custom_memory_field.6600543e.py"
+  },
+  {
+    "revision": "6600543ed5b9e82f1f73cb881a283fea",
+    "url": "/lc-blockly/static/media/custom_prompt_field.6600543e.py"
+  },
+  {
+    "revision": "6600543ed5b9e82f1f73cb881a283fea",
+    "url": "/lc-blockly/static/media/custom_retriever_field.6600543e.py"
+  },
+  {
+    "revision": "6600543ed5b9e82f1f73cb881a283fea",
+    "url": "/lc-blockly/static/media/custom_splitter_field.6600543e.py"
+  },
+  {
+    "revision": "6600543ed5b9e82f1f73cb881a283fea",
+    "url": "/lc-blockly/static/media/custom_tool_field.6600543e.py"
+  },
+  {
+    "revision": "6600543ed5b9e82f1f73cb881a283fea",
+    "url": "/lc-blockly/static/media/custom_util_field.6600543e.py"
+  },
+  {
+    "revision": "6600543ed5b9e82f1f73cb881a283fea",
+    "url": "/lc-blockly/static/media/custom_vectorstore_field.6600543e.py"
+  }
+]);
---
+++ static/media/application.03f03941.py
@@ -0,0 +1,9 @@
+# %%
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+tfds.disable_progress_bar()
